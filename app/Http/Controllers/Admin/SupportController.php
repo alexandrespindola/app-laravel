187c9698024cd2edd6abc3f6f358788305c901aa
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-03-06 08:06:35 +0100

    Atualiza o método index do SupportController para buscar todos os registros de Support

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -1,14 +1,17 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
+use App\Models\Support;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index()
+    public function index(Support $support)
     {
+        $supports = $support->all();
+        dd($supports);
+
         return view('admin.supports.index');
     }
 }
